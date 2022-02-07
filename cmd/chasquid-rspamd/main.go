091a5ce4124d79f2cc6b680e5daebd8e9052d4eb
Denys Vitali <denys@denv.it>
2022-02-07 19:04:17 +0100

    fix: only pass variables if they are not ""

--- cmd/chasquid-rspamd/main.go
+++ cmd/chasquid-rspamd/main.go
@@ -12,18 +12,45 @@ import "github.com/sirupsen/logrus"
 var args struct {
 	RspamdURL string `arg:"-u,--url" help:"rspamd URL"`
 	Auth      string `arg:"-a,--auth" help:"Auth string"`
+	LogToFile string `arg:"-f,--log-file" help:"Log to file"`
+	Debug     *bool  `arg:"-D,--debug"`
 }
 
 func main() {
 	logger := logrus.New()
-	logger.Out = os.Stderr
 	arg.MustParse(&args)
+
+	var outputLogFile *os.File = nil
+	var err error
+	defer func() {
+		if outputLogFile != nil {
+			outputLogFile.Close()
+		}
+	}()
+
+	if args.LogToFile != "" {
+		outputLogFile, err = os.OpenFile(args.LogToFile,
+			os.O_CREATE | os.O_APPEND | os.O_RDWR,
+			0644,
+		)
+		if err != nil {
+			logger.Fatalf("unable to set output log file: %v", err)
+		}
+		logger.Out = outputLogFile
+	} else {
+		logger.Out = os.Stderr
+	}
+
+	if args.Debug != nil && *args.Debug {
+		logger.SetLevel(logrus.DebugLevel)
+	}
+
 	u, err := url.Parse(args.RspamdURL)
 	if err != nil {
 		logger.Fatalf("invalid rspamd URL: %v", err)
 	}
 	if u.Scheme != "http" && u.Scheme != "https" {
-		logger.Fatalf("invalid scheme, only http and https are supported")
+		logger.Fatalf("invalid scheme %s, only http and https are supported", u.Scheme)
 	}
 
 	c, err := rspamd.New(args.RspamdURL)
@@ -31,28 +58,32 @@ func main() {
 		logger.Fatalf("unable to create rspamd client: %v", err)
 	}
 
-	remoteAddr := os.Getenv("REMOTE_ADDR")
-	from := os.Getenv("MAIL_FROM")
-	ehloDomain := os.Getenv("EHLO_DOMAIN")
-	rcpt := os.Getenv("RCPT_TO")
-
 	req := rspamd.ScanRequest{
-		Body: os.Stdin,
-		SourceIP: &remoteAddr,
-		From: &from,
-		Hostname: &ehloDomain,
-		Rcpt: &rcpt,
+		Body:     os.Stdin,
+		SourceIP: envVarOrNil("REMOTE_ADDR"),
+		From:     envVarOrNil("MAIL_FROM"),
+		Hostname: envVarOrNil("EHLO_DOMAIN"),
+		Rcpt:     envVarOrNil("RCPT_TO"),
 	}
 	if args.Auth != "" {
 		req.User = &args.Auth
 	}
-
 
 	res, err := c.Scan(&req)
 	if err != nil {
 		logger.Fatalf("unable to perform scan: %v", err)
 	}
 
+	logger.Debugf("response=%v", res)
+
 	fmt.Printf("X-Spam-Action: %s\n", res.Action)
 	fmt.Printf("X-Spam-Score: %.2f\n", res.Score)
+}
+
+func envVarOrNil(key string) *string {
+	v := os.Getenv(key)
+	if v == "" {
+		return nil
+	}
+	return &v
 }
